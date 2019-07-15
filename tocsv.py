747649ea87b5401c41a27b0a29149f70e6a3b5b7
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2019-07-15 11:18:23 +0200

    tocsv python script updated

--- tocsv.py
+++ tocsv.py
@@ -14,7 +14,7 @@ time = ""
 left = []
 right = []
 
-out.write("FUNCTION;COMMITS;ABORTS;TIME (s);THREADS;ITEMS;OPS;;;FUNCTION;COMMITS;ABORTS;TIME (s);THREADS;ITEMS;OPS\n")
+out.write("FUNCTION;THREADS;ITEMS;OPS;COMMITS;ABORTS;TIME (s);;;FUNCTION;THREADS;ITEMS;OPS;COMMITS;ABORTS;TIME (s);\n")
 
 for line in file:
 	splitted = line.split()
@@ -22,18 +22,18 @@ for line in file:
 		threads = splitted[2]
 		items = splitted[4]
 		operations = splitted[6]
-		run = "RUN4"
+		run = "RUN3"
 	if "pools" in line:
-		run = "RUN5"
+		run = "RUN4"
 	if "Commits" in line:
 		commits = splitted[1]
 		aborts = splitted[3]
 	if "time" in line:
 		time = splitted[4].replace(".",",")
-		if run == "RUN4":
-			left.append(""+run+";"+commits+";"+aborts+";"+time+";"+threads+";"+items+";"+operations)
+		if run == "RUN3":
+			left.append(""+run+";"+threads+";"+items+";"+operations+";"+commits+";"+aborts+";"+time)
 		else:
-			right.append(""+run+";"+commits+";"+aborts+";"+time+";"+threads+";"+items+";"+operations)
+			right.append(""+run+";"+threads+";"+items+";"+operations+";"+commits+";"+aborts+";"+time)
 
 for i in range(0,len(left)):
 	out.write(left[i]+";;;"+right[i]+"\n")
