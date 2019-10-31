19edc96f5a950c562a3e9c3399df1e78026d60c8
Riccardo Valentini <valentiniriccardo05@gmail.com>
2019-10-31 14:50:20 +0100

    Update set_dummy.sh

--- set_dummy.sh
+++ set_dummy.sh
@@ -10,5 +10,5 @@ REPETITIONS_ARRAY=(0 5 10 15 20 25 30 35 40 41 42 43 44 45 50 55 60 65 70)
 			echo " "
 			echo "---- THREADS 1 ITEMS 10 OPS 5 TRANS 1000 PROB 0.5 ----"
 			echo " "
-			./app 1 $I $I 1000 10 1 5 0.5;
+			./app 1 $I 0 1000 10 1 5 0.5;
 		done
