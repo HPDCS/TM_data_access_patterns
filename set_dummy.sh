431ea8d7dd1afd3c3d2ee58158a2a5fc9e37c8bc
Riccardo Valentini <valentiniriccardo05@gmail.com>
2019-10-31 14:42:06 +0100

    Modified set_dummy.sh

--- set_dummy.sh
+++ set_dummy.sh
@@ -8,7 +8,7 @@ REPETITIONS_ARRAY=(1 5 10 15 20 25 30 35 40 41 42 43 44 45 50 55 60 65 70)
 		do
 			let OPS_PT=(OPS*ITEMS)/10
 			echo " "
-			echo "---- THREADS 8 ITEMS 10 OPS 5 TRANS 1000 PROB 0.5 ----"
+			echo "---- THREADS 1 ITEMS 10 OPS 5 TRANS 1000 PROB 0.5 ----"
 			echo " "
-			./app 8 $I $I 1000 10 1 5 0.5;
+			./app 1 $I $I 1000 10 1 5 0.5;
 		done
